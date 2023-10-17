c0d8a8db8413062cea836209c59413b5870160c7
apmorr <144373529+apmorr@users.noreply.github.com>
2023-10-16 21:34:34 -0400

    Update junk.py

--- junk.py
+++ junk.py
@@ -52,3 +52,190 @@ self.printTT()
 
             #self._head = self._head.next
             #self._tail = self._tail.prev
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = self
+        self.prev = self
+
+
+class TurnTracker:
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._count = 0
+
+    def addPlayer(self, player):
+        new_node = Node(player)
+        # print(new_node.data)
+        current = self._head
+        if self._count == 0 and not self._head and not self._tail:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            self._tail.next = new_node
+            new_node.prev = self._tail
+            self._tail = new_node
+        self._count += 1
+
+    def remove(self, data):  ##this works as intended
+        cur = Node(data)
+        temp = self._head
+        # print(cur.data, temp.data, 'hey')
+        if cur == temp:
+            self.head = None
+        temp = self.head
+        # print(cur.data, temp.data, 'hi')
+
+    def nextPlayer(self):  ##does not work yet
+
+        temp = self._head
+        current = None
+
+        if (temp):
+            current = temp.data
+            self.addPlayer(temp.data)
+            self._count -= 1
+            self._prev = temp
+            self._head = temp.next
+        return current
+
+ 
+
+    '''def skipNextPlayer(self):
+        # print(self.L)
+        # temp = self.L[len(self.L)-1]
+        # print(temp)
+        temp = self.L[0]
+        self.L.pop(0)
+        self.L.append(temp)
+        print(self.L)'''
+
+    def numberOfPlayers(self):
+        return self._count
+
+
+    def reverseTurnOrder(self):
+        stack = []
+        temp = self._head
+        while temp:
+            stack.append(temp.data)
+            temp = temp.next
+
+        # Add all the elements in the stack
+        # in a sequence to the stack
+        temp = self._head
+        while temp:
+            temp.data = stack.pop()
+            temp = temp.next
+
+        # Popped all the elements and the
+        # added in the linked list,
+        # in a reversed order.
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        last = self._tail
+        first = self._head
+        
+        elem = []
+        self._head = last
+
+        for x in range(self._count):
+
+            elem.append(last.data)
+            last = last.prev
+            self._head.next = last
+            print(elem)
+        self.printTT()
+
+        last = self._tail
+   
+        self.printTT()
+        self._head = last
+        self._tail = first
+        last = last.prev
+        first = first.next
+        self.printTT()
+        self._head.next = last
+        self._tail.next = first
+        last = last.prev
+        first = first.next
+        self.printTT()
+        self._head.next = last
+        self._tail.next = first
+        last = last.prev
+        first = first.next
+        self.printTT()
+        self._head.next = last
+        self._tail.next = first
+        last = last.prev
+        first = first.next
+        self.printTT()
+        self._head.next = last
+        self._tail.next = first
+        last = last.prev
+        first = first.next
+        self.printTT()
+        self._head.next = last
+        self._tail.next = first
+        self.printTT()
+
+        print('----')
+        '''
+
+
+
+
+
+    def printTT(self):
+
+        cur = self._head
+
+        for x in range(self._count):
+            print(cur.data, end=' ')
+            cur = cur.next
+        print()
+
+
+tt = TurnTracker()
+tt.addPlayer(1)
+tt.addPlayer(2)
+tt.addPlayer(3)
+tt.addPlayer(4)
+tt.addPlayer(5)
+tt.addPlayer(6)
+
+tt.reverseTurnOrder()
+tt.printTT()
+
+'''
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+tt.printTT()
+print('-----')
+tt.reverseTurnOrder()
+print('-----')
+tt.printTT()
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+'''
